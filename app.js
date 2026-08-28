5bf39d9aa9e523775e84d55c84cba41368d44ca6
Abdulrhman Salameh <abdulrahman.salameh10@gmail.com>
2026-08-28 17:41:02 +0300

    Add form validation

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-function Patient(fullName,passWord,dateOfBirth,gender,phone,disease,img){
+function Patient(username, passWord, dateOfBirth, gender, phone, disease, email, img) {
 
-
-    this.fullName = fullName;
+    this.username = username;
     this.passWord = passWord;
     this.dateOfBirth = dateOfBirth;
     this.gender = gender;
     this.phone = phone;
     this.disease = disease;
     this.img = img;
+    this.email = email;
 }
 
 let patients = [];
@@ -17,87 +17,154 @@ if (localStorage.getItem("patients")) {
 }
 
 
-let form=document.getElementById("patientForm");
+let form = document.getElementById("patientForm");
 
-form.addEventListener("submit",function(event){
+form.addEventListener("submit", function (event) {
     event.preventDefault();
 
-    let fullName=document.getElementById("fullName").value;
-    let passWord=document.getElementById("password").value;
-    let dateOfBirth=document.getElementById("dateOfBirth").value;
-    let phone=document.getElementById("phone").value;
-    let disease=document.getElementById("disease").value;
-    let image =document.getElementById("image").value;
+    let username = document.getElementById("username").value;
+    let passWord = document.getElementById("password").value;
+    let dateOfBirth = document.getElementById("dateOfBirth").value;
+    let phone = document.getElementById("phone").value;
+    let disease = document.getElementById("disease").value;
+    let image = document.getElementById("image").value;
+    let email = document.getElementById("email").value;
+
 
     let gender;
-    if(document.getElementById('male').checked )
-    {
-        gender='Male';
+
+    if (document.getElementById('male').checked) {
+        gender = 'Male';
     }
-    else{
-        gender = 'Female'
+    else {
+        gender = 'Female';
     }
 
-    let newPatient= new Patient(fullName,passWord,dateOfBirth,gender,phone,disease,image)
 
-    patients.push(newPatient)
-    
+    if (username.includes(" ")) {
+        alert("Username must not contain spaces");
+        return;
+    }
+
+
+    if (passWord.length <= 8) {
+        alert("Password must be more than 8 characters");
+        return;
+    }
+
+
+    if (!/[0-9]/.test(passWord)) {
+        alert("Password must contain at least one number");
+        return;
+    }
+
+
+    if (!/[A-Z]/.test(passWord)) {
+        alert("Password must contain at least one uppercase letter");
+        return;
+    }
+
+
+    if (!/[!@#$%^&*]/.test(passWord)) {
+        alert("Password must have a special Characters");
+        return;
+    }
+
+
+    if (!/^07[0-9]{8}$/.test(phone)) {
+        alert("Phone number must be 10 digits and start with 07");
+        return;
+    }
+
+
+    if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+        alert("Please enter a valid email");
+        return;
+    }
+
+    for (let i = 0; i < patients.length; i++) {
+
+    if (patients[i].username === username) {
+        alert("User already exists");
+        return;
+    }
+}
+
+
+    let newPatient = new Patient(
+        username,
+        passWord,
+        dateOfBirth,
+        gender,
+        phone,
+        disease,
+        email,
+        image
+    );
+
+    patients.push(newPatient);
+
     localStorage.setItem("patients", JSON.stringify(patients));
-
 
     render();
 });
-    
-    function render(){
-        let container= document.getElementById("patientsContainer")
-        container.innerHTML = "";
-         for(let i=0;i<patients.length;i++){
-
-            let patient=patients[i];
-
-            let card=document.createElement("div");
-            card.className = "patient-card";            
-            
-
-            let img=document.createElement("img");
-            img.src=patient.img
-            card.appendChild(img)
 
 
-            let name=document.createElement("h3");
-            name.textContent=patient.fullName;
-            card.appendChild(name);
+function render() {
 
-            let password=document.createElement("p")
-            password.textContent="password: "+ patient.passWord;
-            card.appendChild(password)
+    let container = document.getElementById("patientsContainer");
+    container.innerHTML = "";
 
+    for (let i = 0; i < patients.length; i++) {
 
-            let birth=document.createElement("p");
-            birth.textContent="Date Of Birth: "+patient.dateOfBirth;
-            card.appendChild(birth);
+        let patient = patients[i];
 
-
-            let gender=document.createElement("p")
-            gender.textContent="Gender: "+ patient.gender;
-            card.appendChild(gender);
+        let card = document.createElement("div");
+        card.className = "patient-card";
 
 
-            let phone=document.createElement("p");
-            phone.textContent="Phone: "+ patient.phone;
-            card.appendChild(phone);
+        let img = document.createElement("img");
+        img.src = patient.img;
+        card.appendChild(img);
 
 
-            let disease=document.createElement("p")
-            disease.textContent="Disease: "+ patient.disease;
-            card.appendChild(disease);
-
-            container.appendChild(card);
+        let name = document.createElement("h3");
+        name.textContent = patient.username;
+        card.appendChild(name);
 
 
+        let password = document.createElement("p");
+        password.textContent = "password: " + patient.passWord;
+        card.appendChild(password);
 
- }}
-    render();
-   
 
-    
+        let birth = document.createElement("p");
+        birth.textContent = "Date Of Birth: " + patient.dateOfBirth;
+        card.appendChild(birth);
+
+
+        let gender = document.createElement("p");
+        gender.textContent = "Gender: " + patient.gender;
+        card.appendChild(gender);
+
+
+        let phone = document.createElement("p");
+        phone.textContent = "Phone: " + patient.phone;
+        card.appendChild(phone);
+
+
+        let disease = document.createElement("p");
+        disease.textContent = "Disease: " + patient.disease;
+        card.appendChild(disease);
+
+
+        let email = document.createElement("p");
+        email.textContent = "Email: " + patient.email;
+        card.appendChild(email);
+
+
+        container.appendChild(card);
+    }
+}
+
+render();
